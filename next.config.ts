464a15de7e2f126548a7cde71266d9a104f19f79
Jemi Arian <Rianpedia.com@gmail.com>
2026-02-20 01:41:25 +0700

    update 10

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   serverExternalPackages: ["better-sqlite3"],
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: false,
+  generateBuildId: async () => {
+    return "fhunpal-build";
+  },
 };
 
 export default nextConfig;
