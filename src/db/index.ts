6479c6e79bde3b2b7d58787681f2b001559c3064
Jemi Arian <Rianpedia.com@gmail.com>
2026-02-23 17:45:02 +0700

    u3

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,13 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 import * as schema from "./schema";
-import * as dotenv from "dotenv";
 
-dotenv.config({ path: ".env.local" });
+const databaseUrl = process.env.DATABASE_URL;
 
-const poolConnection = mysql.createPool(process.env.DATABASE_URL!);
+if (!databaseUrl) {
+    console.error("❌ DATABASE_URL is not defined in environment variables!");
+}
+
+const poolConnection = mysql.createPool(databaseUrl || "");
 
 export const db = drizzle(poolConnection, { schema, mode: "default" });
